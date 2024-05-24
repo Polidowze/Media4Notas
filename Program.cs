7215b369418c0ce282c0a837aff8ac38db376664
Polidowze <167921430+Polidowze@users.noreply.github.com>
2024-05-24 20:28:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int notas;
+int notafinal;
+Console.WriteLine("--------------------Media4Notas--------------------");
+
+Console.WriteLine("Digite as notas:");
+notas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("digite sua nota");
+notas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("digite sua nota");
+notas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("digite sua nota");
+notas = Convert.ToInt32(Console.ReadLine());
+
+if(notas > 5){
+
+    Console.WriteLine("aprovado");
+}
+
+else(notas = 5){
+
+    Console.WriteLine("Recuperação");
+}
+
+if (notas < 5){
+    Console.WriteLine("Rrcuperação");
+}
+
+ int media = nota + (nota + (notafinal / 4)) ;
