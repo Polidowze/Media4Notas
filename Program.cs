0727f65871bea6b7cfaa02d7a3bdf988f70f03cb
Polidowze <167921430+Polidowze@users.noreply.github.com>
2024-06-21 23:40:53 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,70 @@
-﻿int notas;
-int notafinal;
-Console.WriteLine("--------------------Media4Notas--------------------");
+﻿            Console.Clear();
+            void loop()
+{
+                    Console.WriteLine("--------------------Media4Notas--------------------");
 
-Console.WriteLine("Digite as notas:");
-notas = Convert.ToInt32(Console.ReadLine());
+    double n1, n2, n3, n4, media;
+    string resultado;
 
-Console.Write("digite sua nota");
-notas = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("-- Média --\n");
+        Console.WriteLine("Digite as suas notas abaixo.");
 
-Console.Write("digite sua nota");
-notas = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Nota 1: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("digite sua nota");
-notas = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Nota 2: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
 
-if(notas > 5){
+        Console.Write("Nota 3: ");
+        n3 = Convert.ToDouble(Console.ReadLine());
 
-    Console.WriteLine("aprovado");
+        Console.Write("Nota 4: ");
+        n4 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine();
+
+                if (n1 < 0 || n1 > 10
+ ||             n2 < 0 || n2 > 10
+ ||             n3 < 0 || n3 > 10
+ ||             n4 < 0 || n4 > 10)
+            {
+    Console.WriteLine("Digite somente notas entre 0 e 10.");
+            }
+    
+        else
+        {
+    media = (n1 + n2 + n3 + n4) / 4;
+
+         if (media < 5)
+        {
+    Console.ForegroundColor = ConsoleColor. Red;
+    resultado = "Reprovado";
+        }
+   
+        else if (media > 6)
+        {
+    Console.ForegroundColor = ConsoleColor. Green;
+    resultado = "Aprovado";
+        }
+        else
+        {
+    Console.ForegroundColor = ConsoleColor. Blue;
+    resultado = "Em recuperação";
+        }
+
+    Console.WriteLine($"Você ficou com média {media:N1}. Resultado: {resultado}");
+        }
+    Console.WriteLine("Você quer recomeçar o programa? [S/N]");
+    string reposta = Console.ReadLine()!;
+    
+        if(reposta.ToUpper() == "S")
+        {
+    Console.ResetColor();
+        loop();
+        }
+    
+        else{
+
+         }
 }
-
-else(notas = 5){
-
-    Console.WriteLine("Recuperação");
-}
-
-if (notas < 5){
-    Console.WriteLine("Rrcuperação");
-}
-
- int media = nota + (nota + (notafinal / 4)) ;
+                loop();
